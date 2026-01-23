2c478a097609cfda0d0696a5a3353c414227451d
Shiver1155 <hif7f6dyfdt@gmail.com>
2026-01-23 20:14:05 -0300

    fix

--- src/main/java/agendamentoDeClienteBarbearia/dtos/CadastroBarbeiroDTO.java
+++ src/main/java/agendamentoDeClienteBarbearia/dtos/CadastroBarbeiroDTO.java
@@ -4,8 +4,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record CadastroBarbeiroDTO(
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size; // <--- Importante adicionar
+
+public record CadastroBarbeiroDTO(
 
         @NotBlank(message = "Nome é obrigatório")
         String nome,
@@ -13,6 +18,10 @@ public record CadastroBarbeiroDTO(
         @NotBlank(message = "E-mail é obrigatório")
         @Email(message = "Formato de e-mail inválido")
         String email,
+
+        @NotBlank(message = "A senha é obrigatória")
+        @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres") // Segurança básica
+        String senha,
 
         @NotBlank(message = "A especialidade é obrigatória")
         String especialidade // Ex: "Corte", "Barba", "Colorimetria"
--- src/main/java/agendamentoDeClienteBarbearia/service/BarbeiroService.java
+++ src/main/java/agendamentoDeClienteBarbearia/service/BarbeiroService.java
@@ -5,15 +5,19 @@ import jakarta.transaction.Transactional;
 import agendamentoDeClienteBarbearia.dtos.CadastroBarbeiroDTO;
 import agendamentoDeClienteBarbearia.repository.BarbeiroRepository;
 import agendamentoDeClienteBarbearia.model.Barbeiro;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BarbeiroService {
 
     private final BarbeiroRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public BarbeiroService(BarbeiroRepository repository) {
+    public BarbeiroService(BarbeiroRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
@@ -21,10 +25,12 @@ public class BarbeiroService {
         if (repository.existsByEmail(dados.email())) {
             throw new RegraDeNegocioException("Já existe um barbeiro com este e-mail.");
         }
+        String senhaCriptografada = passwordEncoder.encode(dados.senha());
 
         var barbeiro = new Barbeiro();
         barbeiro.setNome(dados.nome());
         barbeiro.setEmail(dados.email());
+        barbeiro.setSenha(senhaCriptografada);
         barbeiro.setEspecialidade(dados.especialidade());
 
         return repository.save(barbeiro);
