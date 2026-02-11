efa23948c240f779eecf51a97d78d42cfca26756
Shiver1155 <hif7f6dyfdt@gmail.com>
2026-02-10 22:19:26 -0300

    implementando contato de whatsapp no responseDTO

--- src/main/java/agendamentoDeClienteBarbearia/dtosResponse/DetalhamentoBarbeiroDTO.java
+++ src/main/java/agendamentoDeClienteBarbearia/dtosResponse/DetalhamentoBarbeiroDTO.java
@@ -8,7 +8,8 @@ public record DetalhamentoBarbeiroDTO(
         String email,
         String especialidade,
         Boolean ativo,
-        Long donoId // ✅ Mudamos de Objeto para ID para evitar Erro 500 (Recursão Infinita)
+        Long donoId ,// ✅ Mudamos de Objeto para ID para evitar Erro 500 (Recursão Infinita)
+        String whatsappContato
 ) {
     public DetalhamentoBarbeiroDTO(Barbeiro barbeiro) {
         this(
@@ -18,7 +19,8 @@ public record DetalhamentoBarbeiroDTO(
                 barbeiro.getEspecialidade() != null ? barbeiro.getEspecialidade() : "Barbeiro",
                 barbeiro.getAtivo(),
                 // Se tiver dono, pega o ID. Se não, é null (caso do próprio dono)
-                barbeiro.getDono() != null ? barbeiro.getDono().getId() : null
+                barbeiro.getDono() != null ? barbeiro.getDono().getId() : null,
+                barbeiro.getWhatsappContato()
         );
     }
 }
