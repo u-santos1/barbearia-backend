ddd27fc3c64f2b94628309e7a19f483b3228c37d
Shiver1155 <hif7f6dyfdt@gmail.com>
2026-03-13 21:55:04 -0300

    fix: add missing exception handlers to TratadorDeErros

--- src/main/java/agendamentoDeClienteBarbearia/infra/TratadorDeErros.java
+++ src/main/java/agendamentoDeClienteBarbearia/infra/TratadorDeErros.java
@@ -1,12 +1,18 @@
 package agendamentoDeClienteBarbearia.infra;
 
 
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class TratadorDeErros {
 
@@ -22,6 +28,20 @@ public class TratadorDeErros {
     public ResponseEntity tratarRegraDeNegocio(RegraDeNegocioException ex) {
         return ResponseEntity.badRequest().body(new DadosErroSimples(ex.getMessage()));
     }
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(EntityNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new DadosErroSimples(exception.getMessage()));
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity tratarErro401(BadCredentialsException exception){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new DadosErroSimples(exception.getMessage()));
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity tratarErro500(Exception e){
+        log.error("Erro interno: ", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new DadosErroSimples("Erro interno. Tente novamente mais tarde"));
+    }
 
     // DTOs internos para a resposta de erro
     private record DadosErroValidacao(String campo, String mensagem) {
@@ -29,6 +49,7 @@ public class TratadorDeErros {
             this(erro.getField(), erro.getDefaultMessage());
         }
     }
+
 
     private record DadosErroSimples(String mensagem) {}
 }
