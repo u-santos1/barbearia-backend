0c70347fd9567e135026a8bb40279e6df2cd5335
Shiver1155 <hif7f6dyfdt@gmail.com>
2026-04-09 21:30:03 -0300

    fix(dto): altera validacao de data para FutureOrPresent para evitar erro de fuso horário

--- src/main/java/agendamentoDeClienteBarbearia/dtos/AgendamentoDTO.java
+++ src/main/java/agendamentoDeClienteBarbearia/dtos/AgendamentoDTO.java
@@ -3,6 +3,7 @@ package agendamentoDeClienteBarbearia.dtos;
 import agendamentoDeClienteBarbearia.StatusAgendamento;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
@@ -13,6 +14,6 @@ public record AgendamentoDTO(
         @NotNull Long barbeiroId,
         @NotNull Long clienteId,
         @NotNull Long servicoId,
-        @NotNull @Future @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime dataHoraInicio,// O único horário que importa na entrada
+        @NotNull @FutureOrPresent @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime dataHoraInicio,// O único horário que importa na entrada
         String observacao
 ) {}
