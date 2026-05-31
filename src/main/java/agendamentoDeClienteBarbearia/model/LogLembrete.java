d4b5f98fa3f3e241b458a7e6c4b16b4fc40ecac3
Shiver1155 <hif7f6dyfdt@gmail.com>
2026-05-31 00:36:41 -0300

    fix(model): adiciona campo status na entidade LogLembrete para corrigir erro na query de KPIs

--- src/main/java/agendamentoDeClienteBarbearia/model/LogLembrete.java
+++ src/main/java/agendamentoDeClienteBarbearia/model/LogLembrete.java
@@ -5,23 +5,36 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Entity
 @Table(name = "log_lembretes")
-@Getter @Setter @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class LogLembrete {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "agendamento_id", nullable = false)
     private Long agendamentoId;
+
+    @Column(name = "regra_id", nullable = false)
     private Long regraId;
+
+    @Column(name = "data_envio", nullable = false)
     private LocalDateTime dataEnvio;
+
+
+    @Column(nullable = false)
+    private String status;
 
     public LogLembrete(Long agendamentoId, Long regraId) {
         this.agendamentoId = agendamentoId;
         this.regraId = regraId;
-        this.dataEnvio = LocalDateTime.now();
+        this.dataEnvio = LocalDateTime.now(ZoneId.of("America/Sao_Paulo"));
+        this.status = "ENVIADO";
     }
 }
